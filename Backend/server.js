b9ad7e1d429ae8b803e6d0ea933ccacd227b8f6d
adi-builds <adityapaul500@gmail.com>
2026-04-23 20:17:31 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import urlRoutes from './routes/url.js';
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods : ["GET","POST","PUT","DELETE"],
+}));
+app.use(express.json());
+
+app.use("/",urlRoutes);
+
+mongoose.connect(process.env.MONGO_URI ).then(()=>{
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.error('Error connecting to MongoDB:', err);
+});
